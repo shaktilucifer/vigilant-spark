f5b0d5f2c0ad7730da2992916bec88ba037d55a7
shakti <shakticse@gmail.com>
2021-05-02 01:03:36 -0400

    Add zircon UI library dependency Added zircon library for UI. Play around with character placement and screen creation using tile grid

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,30 @@
+import org.hexworks.zircon.api.CP437TilesetResources;
+import org.hexworks.zircon.api.SwingApplications;
+import org.hexworks.zircon.api.application.AppConfig;
+import org.hexworks.zircon.api.color.ANSITileColor;
+import org.hexworks.zircon.api.data.Position;
+import org.hexworks.zircon.api.data.Tile;
+import org.hexworks.zircon.api.grid.TileGrid;
+
 public class App {
 
     public static void main(String[] args) {
 
+        TileGrid tileGrid = SwingApplications
+                .startTileGrid(
+                        AppConfig.newBuilder()
+                                .withSize(50, 50)
+                                .withDefaultTileset(CP437TilesetResources.wanderlust16x16())
+                                .build()
+                );
+        tileGrid.draw(
+                Tile.newBuilder()
+                        .buildCharacterTile().withCharacter('@')
+                        .withBackgroundColor(ANSITileColor.BRIGHT_WHITE)
+                        .withForegroundColor(ANSITileColor.BRIGHT_CYAN)
+                        .toBuilder().build(), Position.create(2, 3)
+        );
+
     }
+
 }
