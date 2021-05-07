6be06beea4ebbc806ee6a8808b1fb8b252397ede
Shakti <shakticse@gmail.com>
2021-05-07 13:46:06 -0400

    add hp and cmbt stats

--- src/main/java/Model/Actor.java
+++ src/main/java/Model/Actor.java
@@ -5,4 +5,8 @@ import Model.Enum.CreatureType;
 // Basic actor that can be a player, opponent or any entity that has life
 public class Actor {
     private CreatureType type;
+    private int hitPoints;
+    private int attack;
+    private int defense;
+    
 }
